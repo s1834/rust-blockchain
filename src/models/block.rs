2b0557a3d28ac6f81700925596c2971531b64cdb
s1834 <s18324@protonmail.com>
2023-06-18 06:35:53 +0530

    :sparkles: Generated the hash of a nlock

--- src/models/block.rs
+++ src/models/block.rs
@@ -19,3 +19,15 @@ pub struct Block {
     pub hash: String
 }
 impl Block {}
+
+// Calculate block hash.
+pub fn calculate_hash(&self) -> String {
+    let mut block_data = self.clone();
+    block_data.hash = String::default();
+    let serialized_block_data = serde_json::to_string(&block_data).unwrap();
+    // Calculate and return SHA-256 hash value.
+    let mut hasher = Sha256::new();
+    hasher.update(serialized_block_data);
+    let result = hasher.finalize();
+    format!("{:x}", result)
+}
